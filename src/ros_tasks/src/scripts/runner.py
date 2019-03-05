76315da634c57fcb15f1054341d8cc2842a52bd8
cmcginn <cmcginn@mit.edu>
2019-03-04 20:43:54 -0500

    Commenting runner.py

--- src/ros_tasks/src/scripts/runner.py
+++ src/ros_tasks/src/scripts/runner.py
@@ -10,7 +10,7 @@ class Runner:
   #move in cyclindrical co-ordinates with R = 1, z = 1 and theta varying
 
   def __init__(self):
-
+    #set up publisher node and rate
     self.theta = 0
     self.runner_pub = rospy.Publisher('position', Float32MultiArray, queue_size=10)
     self.r = rospy.Rate(1)
@@ -22,11 +22,12 @@ class Runner:
   def runner(self):
 
     while not rospy.is_shutdown():
-      
+      #move in a circle in increments of pi/16
       x = math.cos(self.theta)
       y = math.sin(self.theta)
       self.theta += math.pi/16
       
+      #use std message type Float32MultiArray to store coordinates
       msg = Float32MultiArray()
       msg.layout.dim.append(MultiArrayDimension())
       msg.layout.dim[0].label = "coords"
@@ -37,6 +38,8 @@ class Runner:
       msg.data[0] = x
       msg.data[1] = y
       msg.data[2] = 1.0
+      
+      #publish msg
       self.runner_pub.publish(msg)
       
       self.r.sleep()
